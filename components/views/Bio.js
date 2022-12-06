cf1700457b12ad624ba70ea36e34784dd73682c9
jadeven <deveneyjoseph@gmail.com>
2022-12-05 15:58:59 -1000

    added Conacts page

--- components/views/Bio.js
+++ components/views/Bio.js
@@ -1,8 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-    <h1>
-        MY NAME IS JOE DEVENEY AND I DESIGNED THIS SITE TO HELP PEOPLE ...
-    </h1>
-  
+  <h1>
+    MY NAME IS JOE DEVENEY AND I DESIGNED THIS SITE TO HELP PEOPLE ...
+  </h1>
 `;
--- components/views/Contact.js
+++ components/views/Contact.js
@@ -2,6 +2,6 @@ import html from "html-literal";
 
 export default () => html`
   <section id="order">
-    <h1>CONTACT ME</h1>
+    <h1>CONTACT ME ANYTIME</h1>
   </section>
 `;
