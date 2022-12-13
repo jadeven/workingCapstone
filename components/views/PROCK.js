b77ac1698c23e8f4bd0d8a2ce968cef74c218c38
jadeven <deveneyjoseph@gmail.com>
2022-12-12 16:16:09 -1000

    added images

---
+++ components/views/PROCK.js
@@ -0,0 +1,29 @@
+import html from "html-literal";
+
+export default () => html`
+  <section >
+    <header id="welcome">
+      <div id="title">
+        <h1>Pyramid Rock</h1>
+      </div>
+      <div id='image'>
+        IMAGE
+      </div>
+    </header>
+  </section>
+<div id ='info'>
+The Kaneohe Marine Corps Base has a beach known as Pyramid Rock. The beach can have rough waves throughout the year as a result of the east trade winds.
+However the waves are the best during the winter months when the winds blow offshore and large north swells wrap into the sandbar.
+
+</div>
+
+<div id='conditions'>
+  <span> temp: </span>
+  <span> wind: </span>
+  <span> temp: </span>
+
+</div>
+
+</section>
+  </section>
+`;
